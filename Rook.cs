b099a94fde9f958020398dcb727c198eb36ae4d3
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2023-12-07 16:43:22 -0700

    Create Rook.cs

---
+++ Rook.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpChessRemake
+{
+    public class Rook : Piece
+    {
+        private int pointValue;
+
+        public Rook(int startingPosition, GlobalVars.PieceColor color) : base(startingPosition, color)
+        {
+            this.pointValue = 5;
+            this.textIcon = "Ro";
+            this.setPieceType(GlobalVars.PieceType.Rook);
+        }
+
+
+        public override void recordPiecePotentialMoves(Chessboard board)
+        {
+            this.resetPotentialMoves();
+            PieceMoveChecks.recordPotentialHorizontalMoves(this, board);
+            PieceMoveChecks.recordPotentialVerticalMoves(this, board);
+        }
+
+
+
+
+
+
+    }
+}
