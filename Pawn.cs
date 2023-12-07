81b951ae551910b3bfdef62a1b9b51463a5670d9
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2023-12-07 16:41:35 -0700

    Create Pawn.cs

---
+++ Pawn.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpChessRemake
+{
+    public class Pawn : Piece
+    {
+        private int pointValue;
+
+        public Pawn(int startingPosition, GlobalVars.PieceColor color) : base(startingPosition, color)
+        {
+            this.pointValue = 1;
+            this.textIcon = "Pa";
+            this.setPieceType(GlobalVars.PieceType.Pawn);
+        }
+
+
+        public override void recordPiecePotentialMoves(Chessboard board)
+        {
+            this.resetPotentialMoves();
+            PieceMoveChecks.potentialPawnMoves(this, board);
+        }
+
+
+
+    }
+}
