e6a228d46501c826d7df8957777a04f1ba354e4f
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2023-12-12 17:14:05 -0700

    Update King.cs

--- King.cs
+++ King.cs
@@ -17,10 +17,12 @@ namespace CSharpChessRemake
             this.setPieceType(GlobalVars.PieceType.King);
         }
 
-
+        // NOTE: The methods that record diagonal and orthagonal moves will only record 1 square away if the piece type is King.
         public override void recordPiecePotentialMoves(Chessboard board)
         {
-
+            this.resetPotentialMoves();
+            PieceMoveChecks.recordPotentialDiagonalMoves(this, board);
+            PieceMoveChecks.recordPotentialOrthagonalMoves(this, board);
         }
     }
 }
