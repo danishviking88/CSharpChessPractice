12510e2580e44d3ea606282192a5ad432e04a41a
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2023-12-15 16:25:05 -0700

    Update Rook.cs

--- Rook.cs
+++ Rook.cs
@@ -10,17 +10,24 @@ namespace CSharpChessRemake
     {
         private int pointValue;
 
-        public Rook(int startingPosition, GlobalVars.PieceColor color) : base(startingPosition, color)
+        public Rook(int startingPosition, GlobalVars.Color color) : base(startingPosition, color)
         {
             this.pointValue = 5;
-            this.textIcon = "Ro";
+            if (this.getColor() == GlobalVars.Color.White)
+            {
+                this.textIcon = "R";
+            }
+            else
+            {
+                this.textIcon = "r";
+            }
             this.setPieceType(GlobalVars.PieceType.Rook);
         }
 
 
         public override void recordPiecePotentialMoves(Chessboard board)
         {
-            this.resetPotentialMoves();
+            this.resetPotentialMovesAndCaptures();
             PieceMoveChecks.recordPotentialOrthagonalMoves(this, board);
         }
 
