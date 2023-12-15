cb26f0ecd360083d2e78fda3e3a12a1872f1c618
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2023-12-15 16:21:15 -0700

    Update King.cs

--- King.cs
+++ King.cs
@@ -10,19 +10,49 @@ namespace CSharpChessRemake
     {
         private int pointValue;
 
-        public King(int startingPosition, GlobalVars.PieceColor color) : base(startingPosition, color)
+
+        public King(int startingPosition, GlobalVars.Color color) : base(startingPosition, color)
         {
             this.pointValue = 0;
-            this.textIcon = "KI";
+            if (this.getColor() == GlobalVars.Color.White)
+            {
+                this.textIcon = "K";
+            }
+            else
+            {
+                this.textIcon = "k";
+            }
             this.setPieceType(GlobalVars.PieceType.King);
         }
 
-        // NOTE: The methods that record diagonal and orthagonal moves will only record 1 square away if the piece type is King.
+      
         public override void recordPiecePotentialMoves(Chessboard board)
         {
-            this.resetPotentialMoves();
+            this.resetPotentialMovesAndCaptures();
+            // NOTE: The methods that record diagonal and orthagonal moves will only record 1 square away if the piece type is King.
             PieceMoveChecks.recordPotentialDiagonalMoves(this, board);
             PieceMoveChecks.recordPotentialOrthagonalMoves(this, board);
+
+            // Only execute if it is the active King's turn.
+            if (this.getColor() == board.getWhoseTurnItIs())
+            {
+                this.deleteMovesPuttingKingInCheck(board);
+            }
+        }
+
+
+        private void deleteMovesPuttingKingInCheck(Chessboard board)
+        {
+            GlobalVars.Color colorOfEnemyPieces = board.getOppositeOfWhoseTurnItIs();
+            board.recordAllPotentialMovesOfOneColor(colorOfEnemyPieces);
+            for (int i = 0; i < GlobalVars.NUM_OF_SQUARES; i++)
+            {
+                if (this.potentialMoves[i] == true && board.globalPotentialCaptures[i] == true)
+                {
+                    this.potentialMoves[i] = false;
+                    this.potentialCaptures[i] = false;
+                }
+            }
         }
     }
 }
